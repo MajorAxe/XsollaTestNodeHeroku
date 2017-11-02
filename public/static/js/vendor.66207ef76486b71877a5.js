4402771ede9bf42d7f6fa466812c27379073474f
MajorAxe <13rgb13@gmail.com>
2017-11-02 13:31:07 +0500

    Ready for production

--- public/static/js/vendor.66207ef76486b71877a5.js
+++ public/static/js/vendor.66207ef76486b71877a5.js
@@ -3682,6 +3682,141 @@ exports.clearImmediate = clearImmediate;
 
 /***/ }),
 
+/***/ "1I9z":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* unused harmony export VueMaskPlugin */
+/* unused harmony export VueMaskDirective */
+var format = function (data, mask) {
+  if (!mask) return data;
+
+  var maskStartRegExp = /^([^#ANX]+)/;
+
+  if (data.length == 1 && maskStartRegExp.test(mask)) {
+    data = maskStartRegExp.exec(mask)[0] + data;
+  }
+
+  var text = '';
+
+  var cOffset = 0;
+
+  for (var i = 0; i < mask.length; i++) {
+    var m = mask.charAt(i);
+    switch (m) {
+      case '#':
+        break;
+      case 'A':
+        break;
+      case '?':
+        break;
+      case 'N':
+        break;
+      case 'X':
+        break;
+      default:
+        data = data.replace(m, '');
+    }
+  }
+  for (var _i = 0, x = 1; x && _i < mask.length; ++_i) {
+    var c = data.charAt(_i - cOffset);
+    var _m = mask.charAt(_i);
+
+    switch (_m) {
+      case '#':
+        if (/\d/.test(c)) {
+          text += c;
+        } else {
+          x = 0;
+        }break;
+      case 'A':
+        if (/[a-z]/i.test(c)) {
+          text += c;
+        } else {
+          x = 0;
+        }break;
+      case 'N':
+        if (/[a-z0-9]/i.test(c)) {
+          text += c;
+        } else {
+          x = 0;
+        }break;
+
+      case '?':
+        cOffset++;break;
+      case 'X':
+        text += c;break;
+      default:
+        text += _m;
+
+        if (c && c !== _m) {
+          data = ' ' + data;
+        }
+
+        break;
+    }
+  }
+  return text;
+};
+
+var trigger = function trigger(el, type) {
+  var e = document.createEvent('HTMLEvents');
+  e.initEvent(type, true, true);
+  el.dispatchEvent(e);
+};
+
+function updateValue(el) {
+  var force = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : false;
+  var value = el.value,
+      _el$dataset = el.dataset,
+      _el$dataset$previousV = _el$dataset.previousValue,
+      previousValue = _el$dataset$previousV === undefined ? "" : _el$dataset$previousV,
+      mask = _el$dataset.mask;
+
+
+  if (force || value && value !== previousValue && value.length > previousValue.length) {
+    el.value = format(value, mask);
+    trigger(el, 'input');
+  }
+
+  el.dataset.previousValue = value;
+}
+
+function updateMask(el, mask) {
+  el.dataset.mask = mask;
+}
+
+var VueMaskDirective = {
+  bind: function bind(el, _ref) {
+    var value = _ref.value;
+
+    updateMask(el, value);
+    updateValue(el);
+  },
+  componentUpdated: function componentUpdated(el, _ref2) {
+    var value = _ref2.value,
+        oldValue = _ref2.oldValue;
+
+
+    var isMaskChanged = value !== oldValue;
+
+    if (isMaskChanged) {
+      updateMask(el, value);
+    }
+
+    updateValue(el, isMaskChanged);
+  }
+};
+
+var VueMaskPlugin = function VueMaskPlugin(Vue) {
+  Vue.directive('mask', VueMaskDirective);
+};
+
+/* harmony default export */ __webpack_exports__["a"] = (VueMaskPlugin);
+
+
+/***/ }),
+
 /***/ "1kS7":
 /***/ (function(module, exports) {
 
@@ -59287,4 +59422,4 @@ function scrollIntoView(container, selected) {
 /***/ })
 
 });
-//# sourceMappingURL=vendor.23327cb6617d2d9dc88f.js.map
+//# sourceMappingURL=vendor.66207ef76486b71877a5.js.map
